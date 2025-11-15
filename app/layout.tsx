bc482f4ab3919204ae887a34cb5bb8d1308d67ae
sasty1 <sokunla33@gmail.com>
2025-11-15 12:01:38 +0100

    Fix: removed NeynarProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
 import "./globals.css";
-import { NeynarProvider } from "@neynar/react";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <NeynarProvider apiKey={process.env.NEXT_PUBLIC_NEYNAR_API_KEY}>
-          {children}
-        </NeynarProvider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
